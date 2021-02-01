e97cd9f5e86e7d6a17d939afdec17d8254e4a042
Akash Kumar <akashnkumar1@gmail.com>
2021-02-01 15:49:07 +0530

    Now user can select his desired pdf file and use this simple audiobook tool

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pyttsx3
 import PyPDF2
+from tkinter.filedialog import askopenfilename
+book = askopenfilename(initialdir='.')
 speech = pyttsx3.init()
-book = open('Read.pdf','rb')
 pdfRead = PyPDF2.PdfFileReader(book)
-numofpage = pdfRead.getNumPages()
+total_page = pdfRead.getNumPages()
 speech.say("Total Number of Page is")
-speech.say(numofpage)
+speech.say(total_page)
 speech.say("Enter the page number")
-readatpage = int(input("Enter the page number: "))
-for num in range(readatpage, numofpage):
-    page = pdfRead.getPage(readatpage)
+read_page = int(input("Enter the page number: "))
+for num in range(read_page, total_page):
+    page = pdfRead.getPage(read_page)
     text = page.extractText()
     speech.say(text)
     speech.runAndWait()
